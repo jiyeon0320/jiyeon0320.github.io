73241cb122c5c3d6392601c550393127be57c3ce
bburnji <bburnji@gmail.com>
2026-03-31 20:28:45 +0900

    route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,22 @@ import PostPreview from './pages/PostPreview'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<PostPreview />} />
-      <Route path="/list" element={<PostListPage />} />
+      <Route
+        path="/"
+        element={<PostPreview />}
+      />
+      <Route
+        path="/posts"
+        element={<PostListPage />}
+      />
+      <Route
+        path="/posts/:slug"
+        element={<PostListPage />}
+      />
+      <Route
+        path="/about"
+        element={<PostListPage />}
+      />
     </Routes>
   )
 }
